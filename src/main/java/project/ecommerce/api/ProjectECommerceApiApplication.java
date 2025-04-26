b22d952072cf730d80136ef0654bfbb559e1b5ac
gybran <charistomarcgybran@students.undip.ac.id>
2025-04-26 17:42:55 +0700

    first-commit

---
+++ src/main/java/project/ecommerce/api/ProjectECommerceApiApplication.java
@@ -0,0 +1,13 @@
+package project.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectECommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectECommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ecommerce/api/ProjectECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
